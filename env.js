ef797c6f8e2196cdafbad55b08f2dc62f134656f
april <52494011+aprilwzh@users.noreply.github.com>
2025-09-10 20:11:06 +0800

    Update env.js

--- env.js
+++ env.js
@@ -1,1 +1,1 @@
-window.GAS_URL = 'https://script.google.com/macros/s/AKfycbzDC2cCx-PgbeaZbhHSClBONmtCjLTQspRNzr1w86i60StZ4nhBYoBwcFmUkVxWWA/exec';
+window.GAS_URL = 'https://script.google.com/macros/s/AKfycbzI8csV8-M5raH4SgPNinor7b5CWAOQ_QWJXJOT0tCGwX9duCwlIlwbWfRmYqfLKDM/exec';
